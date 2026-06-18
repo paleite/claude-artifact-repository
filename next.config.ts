1f9c0f96629f0ab5e2810d623eab091aa373153f
Patrick Aleite <paleite@users.noreply.github.com>
2026-06-18 14:03:19 +0200

    Optimize deps

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: { root: __dirname },
+  reactCompiler: true,
   reactStrictMode: true,
-
   output: "export",
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
